a479ab601b497e8747c69b1ad518af318966ad55
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-26 18:55:37 +0700

    ass 1 done

--- exercises/1.js
+++ exercises/1.js
@@ -5,3 +5,22 @@ Parameter: 1 number, 1 string.
 Output: 1 string
 Contoh: solution(2, "abc") hasilnya menjadi "abcabc"
 */
+const solution = (num, str) => {
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output = output + str
+    };
+    return output;
+};
+
+
+// const solution = (num, str, output="") => {
+//     for (let i = 0; i < num; i++) {
+//         output = output + str
+//     };
+//     return output;
+// };
+
+module.exports = {
+    solution
+};
