d88e3a12b58035a5b3bb12e94c353a93083fe899
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-27 16:58:51 +0700

    ass 3 done

--- exercises/3.js
+++ exercises/3.js
@@ -5,3 +5,31 @@ Parameter: 2 string
 Output: boolean
 Contoh: solution("abc", "a") hasilnya menjadi true
 */
+// const solution = (str1, str2) => {
+//     let i = 0;
+//     do {
+//        let output = str1.includes(str2.charAt(i))
+//        return output
+//        i++
+//     }
+//     while (i <= str1.length);
+// };
+
+ const solution = (str1, str2) => {
+     let i = 0
+     let lowercase = str1.toLowerCase();
+     do {
+         console.log(i)
+        if (str2 === lowercase[i]) {
+            return true
+         }
+         i++
+     }
+     while (i <= lowercase.length);
+     return false
+ };
+
+
+module.exports = {
+    solution
+};
