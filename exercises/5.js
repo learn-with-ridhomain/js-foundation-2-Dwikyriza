e2eaff5f14874ac940975bedc3ac3e888309aedb
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-28 21:50:46 +0700

    ass 5 done

--- exercises/5.js
+++ exercises/5.js
@@ -1,2 +1,15 @@
 // Write a function that takes year(integer) as an input and compute
 // whether the given year is leap year or not
+
+const solution = (year) => {
+    if (year % 4 !== 0) {
+        return false
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false
+    };
+    return true
+};
+
+module.exports = {
+    solution
+};
