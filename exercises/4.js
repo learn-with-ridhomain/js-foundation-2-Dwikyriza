6fab272543db963a4b6655854cfcf47395c742e7
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-28 15:15:42 +0700

    ass 4 done

--- exercises/4.js
+++ exercises/4.js
@@ -5,3 +5,17 @@ Parameter: 2 string
 Output: boolean
 Contoh: solution("abc", "a") hasilnya menjadi 1
 */
+
+const solution = (str1, str2) => {
+    let berak = 0
+    for (i = 0; i < str1.length; i++) {
+        if (str2 === str1.charAt(i)) {
+            berak = berak + 1
+         }
+    }
+    return berak;
+};
+
+module.exports = {
+    solution
+};
