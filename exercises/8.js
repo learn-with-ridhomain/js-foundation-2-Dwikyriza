e6c5b6663cc8ed32e72ccb294fb579976e609e4f
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-29 10:44:43 +0700

    ass 8 done

--- exercises/8.js
+++ exercises/8.js
@@ -1,2 +1,18 @@
 // Write a function that takes a string and remove the characters with odd index
 // Don't forget to return the value
+
+const solution = (str) => {
+    let i = 2;
+    let output = str.charAt(0)
+    while (i <= str.length) {
+        if (i % 2 === 0) {
+            output = output + str.charAt(i)
+        }
+        i++
+    };
+    return output;
+};
+
+module.exports = {
+    solution
+};
