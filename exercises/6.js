53910deca51cb9dea868fe23e3e834f6ecc13d8e
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-29 10:19:52 +0700

    ass 6 done

--- exercises/6.js
+++ exercises/6.js
@@ -1,2 +1,16 @@
 // Write a function that takes a number and return the sum of all its divisor
 // 1 or the number itself are excluded
+
+const solution = (num) => {
+    let output = 0
+    for (i = 2; i < num; i++) {
+        if (num % i === 0) {
+            output = output + i
+         }
+    }
+    return output;
+};
+
+module.exports = {
+    solution
+};
