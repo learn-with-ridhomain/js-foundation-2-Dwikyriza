2f83ceb9149b4437a0a1160395b3aac34a4f12e4
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-03-29 10:33:04 +0700

    ass 7 done

--- exercises/7.js
+++ exercises/7.js
@@ -2,3 +2,20 @@
 // Greataest Common Denominator => Pembagi terbesar
 // Contoh: 4 dan 2 => 4
 // 12 dan 18 => 6
+
+const solution = (num1, num2) => {
+    let i = 0
+    let output = 0
+    do {
+        if (num1 % i === 0 && num2 % i === 0) {
+            output = i
+        }
+        i++
+    }
+    while (i <= num1);
+    return output;
+};
+
+module.exports = {
+    solution
+};
