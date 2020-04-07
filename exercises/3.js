f0de1237aa831701006b49df5bfb778a974bcbc9
dwikyriza <muhammad.dwikyriza@gmail.com>
2020-04-07 17:18:42 +0700

    ass 3 & ass 4 done

--- exercises/3.js
+++ exercises/3.js
@@ -5,3 +5,19 @@ Parameter: 2 string
 Output: boolean
 Contoh: solution("abc", "a") hasilnya menjadi true
 */
+const solution = (str1, str2, i = 0) => {
+    if (i >= str1.length) return false
+    if (str2.includes(str1[i].toLowerCase())) return true
+    return solution(str1, str2, i + 1)
+}
+
+// const solution = (str1, str2) => {
+//     let lowercase = str1.toLowerCase()
+//     const output = (lowercase, str2, i = 0) => {
+//         if (i >= lowercase) return false
+//     }
+// }
+
+module.exports = {
+    solution
+  };
--- exercises/4.js
+++ exercises/4.js
@@ -5,3 +5,15 @@ Parameter: 2 string
 Output: boolean
 Contoh: solution("abc", "a") hasilnya menjadi 1
 */
+
+const solution = (str1, str2, i = 0, output = 0) => {
+    if (i >= str1.length) return output
+    if (str2.includes(str1[i].toLowerCase())) return solution(str1, str2, i + 1, output = output + 1)
+    if (!str2.includes(str1[i].toLowerCase())) return solution(str1, str2, i + 1, output = output + 0)
+    // return solution(str1, str2, i + 1, output = output + 1)
+    // console.log(output)
+}
+
+module.exports = {
+    solution
+  };
